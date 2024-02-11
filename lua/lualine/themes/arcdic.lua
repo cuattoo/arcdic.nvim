46cb7223d95602bf9751cb1217bee1efc3a84b16
cuattoo <b.timoshz@gmail.com>
2024-02-11 17:26:42 +0100

    Updated some code

--- lua/lualine/themes/arcdic.lua
+++ lua/lualine/themes/arcdic.lua
@@ -1,47 +1,47 @@
 local C = require 'arcdic.colors'
 
-local nordic = {}
+local arcdic = {}
 
-nordic.normal = {
+arcdic.normal = {
     a = { bg = C.orange.bright, fg = C.black0, gui = 'bold' },
     b = { bg = C.gray1, fg = C.white1 },
     c = { bg = C.bg_statusline, fg = C.white0 },
 }
 
-nordic.insert = {
+arcdic.insert = {
     a = { bg = C.green.bright, fg = C.black0, gui = 'bold' },
     b = { bg = C.gray1, fg = C.white1 },
     c = { bg = C.bg_statusline, fg = C.white0 },
 }
 
-nordic.command = {
+arcdic.command = {
     a = { bg = C.cyan.bright, fg = C.black0, gui = 'bold' },
     b = { bg = C.gray1, fg = C.white1 },
     c = { bg = C.bg_statusline, fg = C.white0 },
 }
 
-nordic.visual = {
+arcdic.visual = {
     a = { bg = C.red.bright, fg = C.black0, gui = 'bold' },
     b = { bg = C.gray1, fg = C.white1 },
     c = { bg = C.bg_statusline, fg = C.white0 },
 }
 
-nordic.replace = {
+arcdic.replace = {
     a = { bg = C.magenta.bright, fg = C.black0, gui = 'bold' },
     b = { bg = C.gray1, fg = C.white1 },
     c = { bg = C.bg_statusline, fg = C.white0 },
 }
 
-nordic.terminal = {
+arcdic.terminal = {
     a = { bg = C.blue2, fg = C.black0, gui = 'bold' },
     b = { bg = C.gray1, fg = C.white1 },
     c = { bg = C.bg_statusline, fg = C.white0 },
 }
 
-nordic.inactive = {
+arcdic.inactive = {
     a = { bg = C.gray2, fg = C.black0, gui = 'bold' },
     b = { bg = C.gray1, fg = C.white1 },
     c = { bg = C.bg_statusline, fg = C.white0 },
 }
 
-return nordic
+return arcdic
