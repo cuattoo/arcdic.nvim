1a0f3f6dea5bb562eb716a763286a9b4cc07f988
cuattoo <b.timoshz@gmail.com>
2024-02-11 14:51:39 +0100

    Update some code on arcdic.nvim

--- lua/arcdic/init.lua
+++ lua/arcdic/init.lua
@@ -20,7 +20,7 @@ function M.load(opts)
 end
 
 -- Add command to nvim
-vim.api.nvim_create_user_command('Arcdic', function(_)
+vim.api.nvim_create_user_command('arcdic', function(_)
     vim.api.nvim_command 'colorscheme arcdic'
 end, {
     nargs = 1,
