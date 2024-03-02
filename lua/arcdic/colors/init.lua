ce9850eeb184b613612e62080f725781277550e0
cuattoo <b.timoshz@gmail.com>
2024-03-02 12:35:07 +0100

    updated colors

--- lua/arcdic/colors/init.lua
+++ lua/arcdic/colors/init.lua
@@ -33,7 +33,7 @@ function C.extend_palette()
     -- Backgrounds
     C.bg = (O.transparent_bg and C.none) or C.orange.soft
     C.bg_dark = (O.transparent_bg and C.none) or C.orange.soft
-    C.bg_highlight = (O.transparent_bg and C.base.bg) or U.blend(C.magenta.bright, C.blue2, O.cursorline.blend)
+    C.bg_highlight = C.base.bg or U.blend(C.magenta.bright, C.blue2, O.cursorline.blend)
     C.bg_visual = C.bg_highlight
     C.bg_sidebar = (O.transparent_bg and C.none) or C.bg
     C.bg_popup = (O.transparent_bg and C.none) or C.bg
