d80bbb1df24e9a1681bc05d48d0a1b7be5959556
cuattoo <b.timoshz@gmail.com>
2024-03-10 11:28:00 +0100

    Updated colors

--- lua/arcdic/colors/init.lua
+++ lua/arcdic/colors/init.lua
@@ -13,18 +13,18 @@ function C.extend_palette()
     local diff_blend = 0.2
 
     -- Add these for international convenience :)
-    C.grey0 = C.gray0
-    C.grey1 = C.gray1
-    C.grey2 = C.gray2
-    C.grey3 = C.gray3
-    C.grey4 = C.gray4
-    C.grey5 = C.gray5
+    C.grey.g0 = C.gray.g0
+    C.grey.g1 = C.gray.g1
+    C.grey.g2 = C.gray.g2
+    C.grey.g3 = C.gray.g3
+    C.grey.g4 = C.gray.g4
+    C.grey.g5 = C.gray.g5
 
     -- Swap background
     if O.swap_backgrounds then
         local gray0 = C.gray0
-        C.gray0 = C.black1
-        C.black1 = gray0
+        C.gray0 = C.black.b1
+        C.black.b1 = gray0
     end
 
     -- Define some use cases.
@@ -38,11 +38,11 @@ function C.extend_palette()
     C.bg_sidebar = (O.transparent_bg and C.none) or C.bg
     C.bg_popup = (O.transparent_bg and C.none) or C.bg
     C.bg_statusline = C.bg_dark
-    C.bg_selected = U.blend(C.gray2, C.black0, 0.4)
-    C.bg_fold = C.gray2
+    C.bg_selected = U.blend(C.gray.g2, C.black.b0, 0.4)
+    C.bg_fold = C.gray.g2
 
     -- Borders
-    C.border_fg = (O.transparent_bg and not O.bright_border and C.base.bg) or (O.bright_border and C.fg_hightlight) or C.grey3
+    C.border_fg = (O.transparent_bg and not O.bright_border and C.base.bg) or (O.bright_border and C.fg_hightlight) or C.grey.g2
     C.border_bg = (O.transparent_bg and C.none) or C.bg
 
     -- Foregrounds
